2ce44684252b67f6cb50e6e957f55975bd640d76
Jake Mckenzie-Edwards <jake.mckenzieedwards@gmail.com>
2023-03-27 19:00:54 +1100

    commit tree 77d446899c8fd71b92135257c35bcf03eec46cbf

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"]
+const event = surprise
+
+function writeCards(names, event = "surprise"){
+    let thankYouCards =[]
+    for (let i = 0; i < names.length; i++){
+        thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+   return thankYouCards
+}
+
+function countDown(firstNumber){
+    while (firstNumber > 0){
+        console.log(firstNumber)
+        firstNumber -= 1
+    }
+
+    console.log(firstNumber)
+}
